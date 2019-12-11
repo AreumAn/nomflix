16b60b2473c229ace8eeb699985a4278674e9304
AREUM AN <areuman@AREUMs-MacBook-Air.local>
2019-12-11 03:45:05 -0500

    Add header and router

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => (
+    <header>
+        <ul>
+            <a href="/">Movies</a>
+        </ul>
+        <ul>
+            <a href="/tv">TV</a>
+        </ul>
+        <ul>
+            <a href="/search">Search</a>
+        </ul>
+    </header>
+)
