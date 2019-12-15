defa7a18a797c445a0eba0d65d4974691bc6331d
AREUM AN <areuman@AREUMs-MacBook-Air.local>
2019-12-14 22:16:53 -0500

    Add style in Post component

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,23 +3,50 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-const Container = styled.div``;
+const Container = styled.div`
+    font-size: 12px;
+`;
 
 const ImageContainer = styled.div``;
 
-const Image = styled.div``;
+const Image = styled.div`
+    background-image: url(${props => props.bgUrl});
+    height: 180px;
+    background-size: cover;
+    border-radius: 4px;
+    background-position: center center;
+    transition: opacity 0.1s linear;
+`;
 
-const Rating = styled.span``;
+const Rating = styled.span`
+    bottom: 5px;
+    right: 5px;
+    position: absolute;
+    opacity: 0;
+    transition: opacity 0.1s linear;
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+    display: block;
+    margin-bottom: 3px;
+`;
 
-const Year = styled.span``;
+const Year = styled.span`
+    font-size: 10px;
+    color: rgba(255, 255, 255, 0.5);
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie }) => (
     <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
         <Container>
             <ImageContainer>
-                <Image bgUrl={imageUrl} />
+                <Image
+                    bgUrl={
+                        imageUrl
+                        ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+                        : require("../assets/noPosterSmall.png")
+                    }
+                />
                 <Rating>
                     <span role="img" aria-label="rating">
                     ⭐️
@@ -27,7 +54,9 @@ const Poster = ({ id, imageUrl, title, rating, year, isMovie }) => (
                     {rating}/10
                 </Rating>
             </ImageContainer>
-            <Title>{title}</Title>
+            <Title>
+                {title.length > 18 ? `${title.substring(0, 18)}...` : title}
+            </Title>
             <Year>{year}</Year>
         </Container>
     </Link>
